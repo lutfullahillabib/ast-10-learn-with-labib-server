504a16ccbf91b264ebfda0c2695df0b27c3e4b3b
lutfullahillabib <lutfullahillabib@gmail.com>
2022-10-25 15:21:23 +0600

    Done

--- index.js
+++ index.js
@@ -4,41 +4,47 @@ const port = process.env.PORT || 5000;
 const cors = require('cors');
 app.use(cors());
 
-const courses = require('./courses.json');
-const category = require('./category.json');
+const categories = require('./Data/categories.json');
+const courses = require('./Data/courses.json');
 
 
 app.get('/', (req, res) => {
     res.send('Ast-10!');
 });
 
-
+// All Categories
 app.get('/categories', (req, res) => {
-    res.send(category);
+    // console.log(categories);
+    res.send(categories);
 });
 
+// Filter Courses by category
 app.get('/categories/:name', (req, res) => {
     const category = (req.params.name);
 
+    // console.log(category);
     const allCategory = courses.filter(item => item.category === category) || {};
+    // console.log(allCategory);
     res.send(allCategory);
 });
 
+// All Courses
 app.get('/courses', (req, res) => {
+    // console.log(courses);
     res.send(courses);
 });
 
+// Find Single Course by ID
 app.get('/courses/:id', (req, res) => {
     const id = req.params.id;
 
-    const allCourses = courses.find(item => item.id === id) || {};
-    console.log(allCourses);
-    res.send(allCourses);
+    const singleCourses = courses.find(item => item.id === id) || {};
+    console.log(singleCourses);
+    res.send(singleCourses);
 });
 
 
-
 app.listen(port, () => {
-    console.log(`Ast server app listening / running on port ${port}`);
+    console.log(`Ast-10 server app listening / running on port ${port}`);
 });
 
